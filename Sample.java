dea1a89cf4c7e3f442208baa12464eb3c4726a76
ghvijayakumar <92421037+ghvijayakumar@users.noreply.github.com>
2022-09-04 13:44:37 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -2,6 +2,6 @@ class Sample{
     public static void main(String args[]){  
      System.out.println("Hello, this is jenkins pipeline parallel tests");  
 	 System.out.println("Testing auto build");   
-	    System.out.println("this is additional text");
+	    //System.out.println("this is additional text");
     }  
 }  
